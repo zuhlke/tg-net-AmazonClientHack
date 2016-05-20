c42f179524d595d38320ef767358bd44c6a1e0e5
Eadred <eadred.birchenough@zuhlke.com>
2016-05-20 16:29:48 +0100

    Added the client proof of concept code

---
+++ AmazonTestClient/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Linq;
+
+namespace AmazonTestClient
+{
+    public class Program
+    {
+        private const string MY_AWS_ACCESS_KEY_ID = "??????";
+        private const string MY_AWS_SECRET_KEY = "??????";
+        private const string MY_ASSOCIATE_ID = "??????";
+        private const string DESTINATION = "webservices.amazon.co.uk";
+
+        public static void Main()
+        {
+            SignedRequestHelper helper = new SignedRequestHelper(MY_AWS_ACCESS_KEY_ID, MY_AWS_SECRET_KEY, DESTINATION);
+
+            /*
+             * The helper supports two forms of requests - dictionary form and query string form.
+             */
+            String requestUrl;
+            String searchResults;
+
+            IDictionary<string, string> r1 = new Dictionary<string, String>();
+            r1["Service"] = "AWSECommerceService";
+            r1["Operation"] = "ItemSearch";
+            r1["Keywords"] = "the hunger games";
+            r1["SearchIndex"] = "Books";
+            r1["AssociateTag"] = MY_ASSOCIATE_ID;
+
+            requestUrl = helper.Sign(r1);
+            searchResults = FetchResults(requestUrl);
+            
+            string firstAsin = null;
+            using (var memStr = new System.IO.MemoryStream())
+            {
+                using (var writer = new System.IO.StreamWriter(memStr))
+                {
+                    writer.Write(searchResults);
+                    writer.Flush();
+
+                    memStr.Seek(0, System.IO.SeekOrigin.Begin);
+
+                    var doc = new XmlDocument();
+                    doc.Load(memStr);
+
+                    firstAsin = doc.GetElementsByTagName("ASIN")[0].InnerText;
+                }
+
+                if (!string.IsNullOrEmpty(firstAsin))
+                {
+                    IDictionary<string, string> r2 = new Dictionary<string, String>();
+                    r2["ItemType"] = "ASIN";
+                    r2["ItemId"] = firstAsin;
+                    r2["ResponseGroup"] = "Offers";
+                    r2["Operation"] = "ItemLookup";
+                    r2["Service"] = "AWSECommerceService";
+                    r2["AssociateTag"] = MY_ASSOCIATE_ID;
+
+                    var requestUrl2 = helper.Sign(r2);
+                    try
+                    {
+                        var searchResults2 = FetchResults(requestUrl2);
+                    }
+                    catch (Exception ex)
+                    {
+                        int qq = 0;
+                    }
+                    
+                }  
+            }
+
+            
+
+            int q = 0;
+        }
+
+        private static string FetchResults(string url)
+        {
+            using (var wc = new System.Net.WebClient())
+            {
+                try
+                {
+                    return wc.DownloadString(url);
+                }
+                catch (Exception ex)
+                {
+                    int q = 0;
+                    throw;
+                }
+                
+            }
+        }
+    }
+}
---
+++ AmazonTestClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AmazonTestClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zuehlke Technology Group")]
+[assembly: AssemblyProduct("AmazonTestClient")]
+[assembly: AssemblyCopyright("Copyright © Zuehlke Technology Group 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b99fc47a-ba95-458c-847f-3903a3fc7736")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
